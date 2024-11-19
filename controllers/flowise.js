04e567bfa259ad4b1d089a8fffe3841c3eda489c
pablo.rial-gil <pablo.rial-gil@sogeti.com>
2024-11-19 08:40:35 +0100

    Test Upload Github

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,21 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    // Call the Flowise Endpoint
+    const response = await fetch(
+      'http://localhost:3000/api/v1/prediction/e7edc0b8-b25c-4cac-9061-9ea87a5e4c54',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(flowiseData),
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json({ message: data.text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
